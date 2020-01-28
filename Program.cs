2ef3898dc04ebb384dea83d011b4f6d46e858da0
Fher <porrino.fernando@gmail.com>
2020-01-28 17:47:37 +0100

    Version

--- Program.cs
+++ Program.cs
@@ -60,7 +60,7 @@ namespace AutomatedAssignmentValidator
         {
             Terminal.BreakLine();
             Terminal.Write("Automated Assignment Validator: ", ConsoleColor.Yellow);                        
-            Terminal.WriteLine("v1.5.0.3");
+            Terminal.WriteLine("v1.6.0.0");
             Terminal.Write(String.Format("Copyright © {0}: ", DateTime.Now.Year), ConsoleColor.Yellow);            
             Terminal.WriteLine("Fernando Porrino Serrano.");
             Terminal.Write(String.Format("Under the AGPL license: ", DateTime.Now.Year), ConsoleColor.Yellow);            
