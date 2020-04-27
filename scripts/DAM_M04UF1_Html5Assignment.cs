858932fa91faa6e6009029c78cdff39842148b94
Fher <porrino.fernando@gmail.com>
2020-04-27 19:25:25 +0200

    Fix

--- scripts/DAM_M04UF1_Html5Assignment.cs
+++ scripts/DAM_M04UF1_Html5Assignment.cs
@@ -35,21 +35,21 @@ namespace AutoCheck.Scripts{
                 index.Connector.ValidateHTML5AgainstW3C();    //exception if fails, so no score will be computed
 
                 OpenQuestion("Question 1.1", "Validating headers", 1);
-                    EvalQuestion(index.CheckIfNodesMatchesAmount("//h1", 1, Operator.LOWER));
-                    EvalQuestion(index.CheckIfNodesMatchesAmount("//h2", 1, Operator.LOWER));
+                    EvalQuestion(index.CheckIfNodesMatchesAmount("//h1", 1, Operator.GREATEREQUALS));
+                    EvalQuestion(index.CheckIfNodesMatchesAmount("//h2", 1, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 1.2", "Validating paragraphs", 1);
-                    EvalQuestion(index.CheckIfNodesMatchesAmount("//p", 2, Operator.LOWER));
-                    EvalQuestion(index.CheckIfNodesContentMatchesAmount("//p", 1500, Operator.LOWER));
+                    EvalQuestion(index.CheckIfNodesMatchesAmount("//p", 2, Operator.GREATEREQUALS));
+                    EvalQuestion(index.CheckIfNodesContentMatchesAmount("//p", 1500, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 1.3", "Validating breaklines", 1);
-                    EvalQuestion(index.CheckIfNodesMatchesAmount("//p/br", 1, Operator.LOWER));
+                    EvalQuestion(index.CheckIfNodesMatchesAmount("//p/br", 1, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 1.4", "Validating images", 1);
-                    EvalQuestion(index.CheckIfNodesMatchesAmount("//img", 1, Operator.LOWER));
+                    EvalQuestion(index.CheckIfNodesMatchesAmount("//img", 1, Operator.GREATEREQUALS));
                 CloseQuestion();
             CloseQuestion();
 
@@ -58,37 +58,37 @@ namespace AutoCheck.Scripts{
                 contacte.Connector.ValidateHTML5AgainstW3C();    //exception if fails, so no score will be computed
 
                 OpenQuestion("Question 2.1", "Validating text fields", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='text']", 2, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='text']", 2, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.2", "Validating number fields", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='number']", 1, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='number']", 1, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.3", "Validating email fields", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='email']", 1, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='email']", 1, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.4", "Validating radio buttons", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='radio']", 3, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='radio']", 3, Operator.GREATEREQUALS));
                     EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='radio'][@name=(//input[@type='radio']/@name)]", 3));
                     EvalQuestion(contacte.CheckIfSiblingsMatchesAmount("//input[@type='radio'][@checked]", 1));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.5", "Validating select lists", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//select", 1, Operator.LOWER));
-                    EvalQuestion(contacte.CheckIfSiblingsMatchesAmount("//select/option", 3, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//select", 1, Operator.GREATEREQUALS));
+                    EvalQuestion(contacte.CheckIfSiblingsMatchesAmount("//select/option", 3, Operator.GREATEREQUALS));
                     EvalQuestion(contacte.CheckIfSiblingsMatchesAmount("//select/option[@selected]", 1));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.6", "Validating checkboxes", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='checkbox']", 3, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='checkbox']", 3, Operator.GREATEREQUALS));
                     EvalQuestion(contacte.CheckIfNodesMatchesAmount("//input[@type='checkbox'][@name=(//input[@type='checkbox']/@name)]", 3));
                     EvalQuestion(contacte.CheckIfSiblingsMatchesAmount("//input[@type='checkbox'][@checked]", 1));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.7", "Validating textarea fields", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//textarea", 1, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//textarea", 1, Operator.GREATEREQUALS));
                 CloseQuestion();
 
                 OpenQuestion("Question 2.8", "Validating placeholders", 1);
@@ -109,11 +109,11 @@ namespace AutoCheck.Scripts{
                 CloseQuestion();
 
                 OpenQuestion("Question 2.10", "Validating table", 1);
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr[1]/td", 4, Operator.LOWER));
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//td[@colspan=3]", 1, Operator.LOWER));
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr/td[1]/label", 8, Operator.LOWER));
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr/td[2]/label", 1, Operator.LOWER));
-                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr/td[3]/label", 5, Operator.LOWER));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr[1]/td", 4, Operator.GREATEREQUALS));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//td[@colspan=3]", 1, Operator.GREATEREQUALS));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr/td[1]/label", 8, Operator.GREATEREQUALS));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr/td[2]/label", 1, Operator.GREATEREQUALS));
+                    EvalQuestion(contacte.CheckIfNodesMatchesAmount("//tr/td[3]/label", 5, Operator.GREATEREQUALS));
                     EvalQuestion(contacte.CheckIfTablesIsConsistent("//table"));                                              
                 CloseQuestion();
 
