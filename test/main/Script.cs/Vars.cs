6220c9281a7c5bf353f38413e1161ec5f25158f9
FherStk <porrino.fernando@gmail.com>
2021-01-22 15:38:09 +0100

    Splittin script test into different folders (was too large)

---
+++ test/main/Script.cs/Vars.cs
@@ -0,0 +1,119 @@
+/*
+    Copyright © 2021 Fernando Porrino Serrano
+    Third party software licenses can be found at /docs/credits/credits.md
+
+    This file is part of AutoCheck.
+
+    AutoCheck is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AutoCheck is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with AutoCheck.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.IO;
+using AutoCheck.Core.Exceptions;
+using NUnit.Framework;
+
+namespace AutoCheck.Test
+{    
+    [Parallelizable(ParallelScope.All)]    
+    public class Vars : Test
+    {        
+        private string _user = AutoCheck.Core.Utils.ConfigFile("gdrive_account.txt");
+        private string _secret = AutoCheck.Core.Utils.ConfigFile("gdrive_secret.json");    
+        
+        [OneTimeSetUp]
+        public virtual void StartUp() 
+        {
+            SamplesScriptFolder = GetSamplePath(Path.Combine("script", "vars"));            
+        }
+
+        protected override void CleanUp(){
+            //TODO: Abort execution and display an error message if there's no PSQL, remote host, etc...
+
+            //Clean temp files
+            var dir = Path.Combine(GetSamplePath("script"), "temp", "vars");
+            if(Directory.Exists(dir)) Directory.Delete(dir, true);       
+
+            //Clean logs
+            var logs = Path.Combine(AutoCheck.Core.Utils.AppFolder, "logs", "vars");
+            if(Directory.Exists(logs)) Directory.Delete(logs, true);                      
+        }
+
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_DEFAULT_VARS()
+        {  
+           Assert.DoesNotThrow(() => new AutoCheck.Core.Script(GetSampleFile("vars_ok1.yaml")));                 
+        }
+
+        [Test, Category("Vars"), Category("Remote")]
+        public void ParseVars_COMPUTED_OPPERATION()
+        {  
+            //NOTE: needs a remote GNU users to work (usuario@usuario)
+            var s = new AutoCheck.Core.Script(GetSampleFile("vars_ok5.yaml"));             
+            Assert.AreEqual("Running script vars_ok5 (v1.0.0.0):\r\n   Running opperation 1+2+3: OK", s.Output.ToString());                          
+        }
+        
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_COMPUTED_REGEX()
+        {  
+            Assert.DoesNotThrow(() => new AutoCheck.Core.Script(GetSampleFile("vars_ok2.yaml")));                                           
+        }
+ 
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_TYPED_SIMPLE()
+        {                         
+            Assert.DoesNotThrow(() => new AutoCheck.Core.Script(GetSampleFile("vars_ok3.yaml")));              
+        }
+
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_SCOPE_LEVEL1()
+        {                         
+            Assert.DoesNotThrow(() => new AutoCheck.Core.Script(GetSampleFile("vars_ok4.yaml")));              
+        }
+               
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_INVALID_DUPLICATED()
+        {  
+            Assert.Throws<DocumentInvalidException>(() => new AutoCheck.Core.Script(GetSampleFile("vars_ko1.yaml")));            
+        }
+
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_NOTEXISTS_SIMPLE()
+        {  
+            Assert.Throws<VariableNotFoundException>(() => new AutoCheck.Core.Script(GetSampleFile("vars_ko2.yaml")));           
+        }
+ 
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_REGEX_NOTAPPLIED()
+        {  
+            Assert.Throws<RegexInvalidException>(() => new AutoCheck.Core.Script(GetSampleFile("vars_ko3.yaml")));
+        }
+
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_REGEX_NOVARNAME()
+        {  
+            Assert.Throws<VariableNotFoundException>(() => new AutoCheck.Core.Script(GetSampleFile("vars_ko4.yaml")));
+        }
+
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_REGEX_NOTEXISTS()
+        {  
+            Assert.Throws<VariableNotFoundException>(() => new AutoCheck.Core.Script(GetSampleFile("vars_ko5.yaml")));
+        }
+
+        [Test, Category("Vars"), Category("Local")]
+        public void ParseVars_SCOPE_NOTEXISTS()
+        {  
+            Assert.Throws<VariableNotFoundException>(() => new AutoCheck.Core.Script(GetSampleFile("vars_ko6.yaml")));
+        }
+    }
+}
