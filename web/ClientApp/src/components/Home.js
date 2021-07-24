cd86eb6f6f7d77018691269a78213db0f920ef34
FherStk <porrino.fernando@gmail.com>
2021-07-24 19:15:31 +0200

    Full demo is working but woth no action on file opening

--- web/ClientApp/src/components/Home.js
+++ web/ClientApp/src/components/Home.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
-import { FullFileBrowser } from 'chonky';
 import { setChonkyDefaults } from 'chonky';
 import { ChonkyIconFA } from 'chonky-icon-fontawesome';
+import { VFSBrowser } from './VFSBrowser/VFSBrowser';
 
-export const MyFileBrowser = () => {
-  const files = [
-      { id: 'lht', name: 'Projects', isDir: true },
-      {
-          id: 'mcd',
-          name: 'chonky-sphere-v2.png',
-          thumbnailUrl: 'https://chonky.io/chonky-sphere-v2.png',
-      },
-  ];
-  const folderChain = [{ id: 'xcv', name: 'Demo', isDir: true }];
-  return (
-      <div style={{ height: 300 }}>
-          <FullFileBrowser files={files} folderChain={folderChain} />
-      </div>
-  );
-};
+setChonkyDefaults({ iconComponent: ChonkyIconFA });
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -28,10 +13,8 @@ export class Home extends Component {
       <div>
         <h1>Wellcome to AutoCheck!</h1>
         <p>Step 1 - Select the AutoCheck's YAML script that you want to launch:</p>
-        <MyFileBrowser></MyFileBrowser>
+        <VFSBrowser instanceId="1" />
       </div>
     );
   }
 }
-
-setChonkyDefaults({ iconComponent: ChonkyIconFA });
