856a50621df7abbf14c4959070c2e7ab60281bff
FherStk <porrino.fernando@gmail.com>
2021-12-05 11:22:31 +0100

    run scripts has been updated to compile the app

--- terminal/run.bat
+++ terminal/run.bat
@@ -1,1 +1,4 @@
+@Echo off
+dotnet build -c Release
+@Echo on
 dotnet bin/Release/net6.0/AutoCheck.Terminal.dll %1 %2
--- terminal/run.sh
+++ terminal/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+dotnet build -c Release > /dev/null 2>&1
 dotnet bin/Release/net6.0/AutoCheck.Terminal.dll $1 $2
