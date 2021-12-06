c3fbdf591caf431b77e718ca27f67d44df4aa337
FherStk <porrino.fernando@gmail.com>
2021-12-06 18:37:16 +0100

    Compiling before restarting

--- terminal/utils/restart.bat
+++ terminal/utils/restart.bat
@@ -1,5 +1,6 @@
 @echo off
 :: The timeout is used to avoid output mixing between processes
 timeout /t 1 /nobreak > NUL 
-dotnet run -nu %1
+dotnet build -c Release
+dotnet bin/Release/net6.0/AutoCheck.Terminal.dll -nu %1 %2
 @echo on
