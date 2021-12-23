7e375503bf7f1520546e2244a49b633afb24ddef
FherStk <porrino.fernando@gmail.com>
2021-12-23 14:59:47 +0100

    Removing React and switching to MVC (sorry, I have no more time for experiments)

---
+++ web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AutoCheck.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
