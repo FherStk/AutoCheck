cf54449ba5a720fdd5d5420b3018358d80045e7c
FherStk <porrino.fernando@gmail.com>
2022-01-30 12:49:57 +0100

    Target is beeing displayed correctly

--- web/Program.cs
+++ web/Program.cs
@@ -28,7 +28,7 @@ else
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
--- web/wwwroot/js/index.js
+++ web/wwwroot/js/index.js
@@ -13,6 +13,46 @@ function showMore(node){
     $(node).next().show();
 }
 
+function run(){
+    $("#mode, #script, #target, #run").prop( "disabled", true );  
+    $("#step-3").show();  
+    $("#log > div").empty();
+    $("#log > img").show();
+
+    $.post("/home/Run", { script: $("#script").val(), target: $("#target").val()}, function(data){                        
+        $("#log > img").hide();
+
+        //TODO: this must be append in async mode   
+        $.each(data, function(i) {      
+            //Multiple log files  
+            var logSelector = "#log > div";                                   
+            $(logSelector).append(
+                '<div class="collapsable">\
+                    <div onclick="showMore(this);" class="header' + (i==0 ? ' hidden' : '') + '">Display more...</div>\
+                    <div class="content' + (i>0 ? ' hidden' : '') + '"></div>\
+                </div>'
+            );
+
+            $.each(this, function() {                
+                var lastLogSelector = logSelector + " > .collapsable:last-child > .content";
+
+                //TODO: this should come in two lines
+                if(this.Text != null && this.Text.startsWith("ERROR:")){
+                    $(lastLogSelector).append('<label class="'  + this.Style + '">ERROR:</label><br>');
+                    this.Text = this.Text.replace("ERROR:", "").replace(/^\n|\n$/g, '');
+                }
+                
+                $(lastLogSelector).append('<label class="'  + this.Style + '"><xmp>' + (this.Indent == null ? "" : this.Indent) +  (this.Text == null ? "" : this.Text) + '</xmp></label>');                        
+                if(this.BreakLine || this.Style == null) $(lastLogSelector).append('<br>');
+            });
+
+            $(logSelector).append('<br>');
+        });
+    });
+
+    $("#run").prop( "disabled", false ); 
+}
+
 $(function(){  
     $.post("/home/CheckForUpdate", function(data){  
         if(data) $("#update-card").show();
@@ -32,24 +72,25 @@ $(function(){
     });
 
     $("#mode").change(function() {
-        $.post("/home/GetScripts", { mode: this.value}, function(data){                     
-            $("#script").empty();
-            $("#script").append('<option value="none" selected>- Select -</option>');
+        $("#step-2").hide();
+        
+        if($("#mode").val() == 'none') $("#script").hide();
+        else{
+            $.post("/home/GetScripts", { mode: this.value}, function(data){                     
+                $("#script").empty();
+                $("#script").append('<option value="none" selected>- Select -</option>');
 
-            $.each(data, function() {                        
-                $("#script").append('<option value="'  + this.path + '">[' + this.source.toLowerCase() + '] ' + this.name + '</option>');
+                $.each(data, function() {                        
+                    $("#script").append('<option value="'  + this.path + '">[' + this.source.toLowerCase() + '] ' + this.name + '</option>');
+                });
+                            
+                $("#script").show();
             });
-            
-            $("#step-2").hide();
-            if(data.length == 0) $("#script").hide();
-            else $("#script").show();
-        });
+        }
     });
 
     $("#script").change(function() {
-        var mode = $("#mode").val();
-        $("#step-2").show();
-
+        var mode = $("#mode").val();        
         if(mode == "single"){
             $("#info-batch").hide();
             $("#info-single").show();
@@ -63,14 +104,12 @@ $(function(){
         $.post("/home/GetTargetData", {script: $("#script").val()}, function(data){                  
             $("#target").empty();
             
+            var rows = 0;
             Object.keys(data).forEach(function(key) {
-                if(key == "os") $("#target").append('<label for"' + key + '">' + key + ': </label><select id="' + key + '" name="' + key + '"><option value="GNU">GNU/Linux</option><option value="MAC">Mac OS</option><option value="WIN">Windows</option></select>');
-                else if(key == "vars"){
-                    $("#target").append("<div id='vars'></div>");
-                    
+                if(key == "os") $("#target").append('<tr><td><label for"' + key + '">' + key + ': </label></td><td><select id="' + key + '" name="' + key + '"><option value="GNU">GNU/Linux</option><option value="MAC">Mac OS</option><option value="WIN">Windows</option></select></td><td></td></tr>'); //$("#target").append('<label for"' + key + '">' + key + ': </label><select id="' + key + '" name="' + key + '"><option value="GNU">GNU/Linux</option><option value="MAC">Mac OS</option><option value="WIN">Windows</option></select>');
+                else if(key == "vars"){                    
                     Object.keys(data.vars).forEach(function(key) {
-                        $("#vars").append('<label for"' + key + '">' + key + ': </label><input type="text" id="' + key + '" name="' + key + '" placeholder="Some data" />');
-                        $("#vars").append('<br>');
+                        $("#vars").append('<td><label for"' + key + '">' + key + ': </label></td><td><input type="text" id="' + key + '" name="' + key + '" class="var" placeholder="Some data" /></td><td></td></tr>');
                     });
                 }
                 else{
@@ -94,60 +133,33 @@ $(function(){
                             break;                               
                     }
 
-                    $("#target").append('<label for"' + key + '">' + key + ': </label><input type="text" id="' + key + '" name="' + key + '" placeholder="' + placeholder + '" />');
-                }
+                    $("#target").append('<tr><td><label for"' + key + '">' + key + ': </label></td><td><input type="text" id="' + key + '" name="' + key + '" placeholder="' + placeholder + '" /></td><td></td></tr>');
+                };
 
-                $("#target").append('<br>');
+                rows++;
             });            
+                        
+            $("#target > tr:not(:first)").each(function() {                        
+                $(data).children("td").last().remove();
+            });
+
+            var runHolder = $("#target > tr").first().children("td").last();
+            runHolder.attr("rowspan", rows);
+            runHolder.append('<input id="run" onclick="run()" type="button" value="Run" disabled="true" />');
             
-            // $("#step-2").hide();
-            // if(data.length == 0) $("#script").hide();
-            // else $("#script").show();
+            $("#step-2").show();
         });
     });  
 
     $("#target").keyup(function() {
-        if(this.value.length > 0) $("#run").prop( "disabled", false ); 
-        else $("#run").prop( "disabled", true ); 
-    });  
-
-    $("#run").click(function() {
-        $("#mode, #script, #target, #run").prop( "disabled", true );  
-        $("#step-3").show();  
-        $("#log > div").empty();
-        $("#log > img").show();
-
-        $.post("/home/Run", { script: $("#script").val(), target: $("#target").val()}, function(data){                        
-            $("#log > img").hide();
-
-            //TODO: this must be append in async mode   
-            $.each(data, function(i) {      
-                //Multiple log files  
-                var logSelector = "#log > div";                                   
-                $(logSelector).append(
-                    '<div class="collapsable">\
-                        <div onclick="showMore(this);" class="header' + (i==0 ? ' hidden' : '') + '">Display more...</div>\
-                        <div class="content' + (i>0 ? ' hidden' : '') + '"></div>\
-                    </div>'
-                );
-
-                $.each(this, function() {                
-                    var lastLogSelector = logSelector + " > .collapsable:last-child > .content";
-
-                    //TODO: this should come in two lines
-                    if(this.Text != null && this.Text.startsWith("ERROR:")){
-                        $(lastLogSelector).append('<label class="'  + this.Style + '">ERROR:</label><br>');
-                        this.Text = this.Text.replace("ERROR:", "").replace(/^\n|\n$/g, '');
-                    }
-                    
-                    $(lastLogSelector).append('<label class="'  + this.Style + '"><xmp>' + (this.Indent == null ? "" : this.Indent) +  (this.Text == null ? "" : this.Text) + '</xmp></label>');                        
-                    if(this.BreakLine || this.Style == null) $(lastLogSelector).append('<br>');
-                });
-
-                $(logSelector).append('<br>');
-            });
+        var disabled = false;
+        $(this).find("input,select").each(function(){
+            if($(this).val() == ""){
+                disabled = true;
+                return false;
+            }             
         });
 
-        $("#run").prop( "disabled", false ); 
-    });
+        $("#run").prop("disabled", disabled); 
+    });  
 });
